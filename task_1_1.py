8cac49958e15c6bf91107bae46a01e1d26f73c64
Cakeboyl <97858497+Cakeboyl@users.noreply.github.com>
2022-01-23 14:58:15 +0300

    Delete task_1_1.py

--- task_1_1.py
+++
@@ -1,6 +0,0 @@
-duration = int(input())
-days = duration // 86400
-hours = (duration - days*86400)//3600
-minutes = (duration - days*86400 - hours*3600)//60
-seconds = duration - days*86400 - hours*3600 - minutes*60
-print(days,'дн',hours,'час',minutes,'мин',seconds,'сек')
